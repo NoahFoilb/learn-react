0d20a6a3cc53df74a70d86fd603177607a00c18a
Noah Foilb <nofoilb14@gmail.com>
2024-02-09 13:25:16 -0500

    020824

--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,55 +1,61 @@
+
+function Portfolio({Name,ImageLink, Profession, AwardsNum, Awards, Discovered}) {
+  return (
+<section className="profile">
+        <h2>{Name}</h2>
+        <img
+          className="avatar"
+          src={ImageLink}
+          alt={Name}
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            {Profession}
+          </li>
+          <li>
+            <b>{"Awards: " + AwardsNum} </b>
+            ({Awards})
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {Discovered}
+          </li>
+        </ul>
+      </section>
+  );
+}
+
+
+
+
 export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+
+
+      <Portfolio
+        Name = {'Maria Skłodowska-Curie'}
+        ImageLink= {'https://i.imgur.com/szV5sdGs.jpg'}
+        Profession= {'physicist and chemist'}
+        AwardsNum={4}
+        Awards={'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal'}
+        Discovered={'polonium (element)'}
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
+
+      <Portfolio
+        Name = {'Katsuko Saruhashi'}
+        ImageLink= {'https://i.imgur.com/YfeOqp2s.jpg'}
+        Profession= {'geochemist'}
+        AwardsNum={2}
+        Awards={'Miyake Prize for geochemistry, Tanaka Prize'}
+        Discovered={'a method for measuring carbon dioxide in seawater'}
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+
+
     </div>
   );
 }
